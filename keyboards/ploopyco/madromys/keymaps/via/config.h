a2b6281855f122591af4da8784dd6b30bf911de2
keansteeves <74686044+keansteeves@users.noreply.github.com>
2026-05-19 09:32:46 -0600

    Add files via upload

---
+++ keyboards/ploopyco/madromys/keymaps/via/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define TAPPING_TERM 250
+#define TAPPING_TERM_PER_KEY
+
+#define COMBO_VARIABLE_LEN
+
+#define TAPPING_TOGGLE 1
+
+#define MOUSE_EXTENDED_REPORT
+
+#define PLOOPY_DPI_OPTIONS { 800, 1200, 1600 }
+#define PLOOPY_DPI_DEFAULT 1
+
+//PMW-3360 The CPI range is 100-12000, in increments of 100. 
